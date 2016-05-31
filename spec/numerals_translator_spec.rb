3b625ca82b50ef994f788127a94264f521b321e2
ha <ha@mmj.vn>
2016-05-31 14:44:39 +0700

    Add spec.

--- spec/numerals_translator_spec.rb
+++ spec/numerals_translator_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe 'Numerals Translator' do
+  require_relative 'cases/numerals_translator_cases'
+
+  NumeralsTranslatorCases::INVALID.each do |invalid_case|
+    it "should raise exception `invalid number` if user passed #{invalid_case}" do
+      expect { NumeralsTranslator.translate(invalid_case) }.
+        to raise_error(InvalidNumber)
+    end
+  end
+
+  NumeralsTranslatorCases::VALID.each do |number, text|
+    it "should return `#{text}` if user passed `#{number}`" do
+      expect( NumeralsTranslator.translate(number) ).
+        to eq(text)
+    end
+  end
 end
