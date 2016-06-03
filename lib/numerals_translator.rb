8279ced75d563850800e120a47affee449418605
ha <ha@mmj.vn>
2016-06-03 16:05:21 +0700

    Fix conflicts after reverted.

--- lib/numerals_translator.rb
+++ lib/numerals_translator.rb
@@ -1,38 +1,8 @@
-require_relative 'exceptions/invalid_number'
-require_relative 'numerals_translator/words_mapping'
+require 'exceptions/invalid_number'
+require 'numerals_translator/words_mapping'
 
 module NumeralsTranslator
   def self.translate(number)
-    raise InvalidNumber unless number.to_s =~ NumeralsTranslator::VALID_NUMERAL
-
-    number = number.to_i
-
-    return "zero" if number == 0
-
-    "#{prefix(number)}#{to_word(number.abs)}".strip
-  end
-
-  def self.prefix(number)
-    "negative " if number < 0
-  end
-
-  def self.to_word(number)
-    NumeralsTranslator::WORDS_MAPPING.each do |unit, word|
-      return if number == 0
-
-      return word if (number < 10) && (number/unit > 0)
-
-      if (number < 100) && (number/unit > 0)
-        if (number%unit == 0)
-          return word
-        else
-          return "#{word} #{to_word(number%unit)}"
-        end
-      end
-
-      if (number/unit > 0)
-        return "#{to_word(number/unit)} #{word} #{to_word(number%unit)}"
-      end
-    end
+    raise InvalidNumber unless number.is_a? Integer
   end
 end
