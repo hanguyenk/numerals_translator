91f1add7aba9a6b8811759c60643da70fb738b10
ha <ha@mmj.vn>
2016-06-03 16:08:06 +0700

    Return zero if user passed 0.

--- lib/numerals_translator.rb
+++ lib/numerals_translator.rb
@@ -4,5 +4,7 @@ require 'numerals_translator/words_mapping'
 module NumeralsTranslator
   def self.translate(number)
     raise InvalidNumber unless number.is_a? Integer
+
+    return "zero" if number == 0
   end
 end
