f204a4572fc76771d4b0325a0d8c1bf6b2a10366
ha <ha@mmj.vn>
2016-06-03 16:20:36 +0700

    Add translator engine.

--- lib/numerals_translator.rb
+++ lib/numerals_translator.rb
@@ -13,5 +13,22 @@ module NumeralsTranslator
   end
 
   def self.to_word(number)
+    NumeralsTranslator::WORDS_MAPPING.each do |unit, word|
+      return if number == 0
+
+      return word if (number < 10) && (number/unit > 0)
+
+      if (number < 100) && (number/unit > 0)
+        if (number%unit == 0)
+          return word
+        else
+          return "#{word} #{to_word(number%unit)}"
+        end
+      end
+
+      if (number/unit > 0)
+        return "#{to_word(number/unit)} #{word} #{to_word(number%unit)}"
+      end
+    end
   end
 end
